b08cacaba2aa94323556ce753c36fa1ff6a77bd1
KEIHAAN382 <85694100+Keihaan382@users.noreply.github.com>
2024-02-12 21:56:57 -0300

    Update fxmanifest.lua

--- k382_Velintes_Day/fxmanifest.lua
+++ k382_Velintes_Day/fxmanifest.lua
@@ -3,11 +3,10 @@ game "gta5"
 
 this_the_map "yes"
 
-PrincipalDescription "This resource has been created upon request and exclusively by the FiveM - Nevermore server - it will be respected as such forever."
-Description "This resource contains vanilla file modifications - they do not affect any map type in the game-"
-Description2 "This resource contains models - animation fisica - uv animation - the resource is fully optimized and 99% tested-"
-Description3 "This resource does not conflict with any type of map"
+PrincipalDescription "This resource has been created for FiveM servers"
+Description "This resource contains models created by Keihaan382"
+Description2 "This resource does not conflict with any type of map"
+Description3 "This resource may not be shared, sold, or published without giving credit to the creator."
+
+Note "This work © 2024 by keihaan382 is licensed under Attribution-ShareAlike 4.0 International"
 Contact "@keihaan382 All Socials Media - https://linktr.ee/keihaan382 - Discord: https://discord.com/invite/CaNzpjZ7nS "
-
-Grateful "The creator thanks Nevermore for the absolute trust placed in him to realize his work."
-
